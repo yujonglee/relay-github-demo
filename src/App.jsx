2970fdc746685b9069f5ac63404468657834312c
yujong-lee <yujonglee@g.skku.edu>
2021-09-04 02:26:34 +0900

    Replace loadQuery with useQueryLoader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Suspense } from 'react';
-import { loadQuery } from 'react-relay/hooks';
+import { useQueryLoader } from 'react-relay/hooks';
 
 import Repo from './components/Repo';
 import { RepositoryNameQuery } from './queries/github';
 import RelayEnvironment from './relay/RelayEnvironment';
 
-const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery);
+const App = () => {
+  const [queryReference] = useQueryLoader(RelayEnvironment, RepositoryNameQuery);
 
-const App = () => (
-  <Suspense fallback="Loading...">
-    <Repo queryReference={preloadedQuery} />
-  </Suspense>
-);
+  return (
+    <Suspense fallback="Loading...">
+      <Repo queryReference={queryReference} />
+    </Suspense>
+  );
+};
 
 export default App;
